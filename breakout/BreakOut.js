f78990abc0618f645603313e620cc9c46f0e6322
Taylor Reid <trgit@hoghill.net>
2021-06-20 18:19:25 -0700

    code to return from breakout

--- breakout/BreakOut.js
+++ breakout/BreakOut.js
@@ -1,11 +1,14 @@
 // Collect user and breakout names, then launch breakout
 // Run this macro NOT as GM
 const userId = args[1] || game.userId;           // id of the user to send to breakout
-const rmStr = args[0].hashCode();                // hash the breakout room name
-const gmStr = game.world.data.title.hashCode();  // hash the world title
-let x = new Date();
-x.setTime(x.valueOf() + ( 60000 * x.getTimezoneOffset() ));
-const dtStr = x.toDateString().hashCode();       // hash the current date in GMT 
-const breakoutRoom = rmStr + gmStr + dtStr;      // use combined hashes as room name
+let breakoutRoom = args[0];                      // breakout room name
+if (breakoutRoom) {                              // if room name blank, exit breakout
+    const rmStr = args[0].hashCode();                // hash the breakout room name
+    const gmStr = game.world.data.title.hashCode();  // hash the world title
+    let x = new Date();
+    x.setTime(x.valueOf() + ( 60000 * x.getTimezoneOffset() ));
+    const dtStr = x.toDateString().hashCode();       // hash the current date in GMT 
+    breakoutRoom = rmStr + gmStr + dtStr;      // use combined hashes as room name
+}
 let RunBreakOut = game.macros.getName("RunBreakOut");
 RunBreakOut.execute(userId, breakoutRoom);
